b0b95d2238eb4a04380ba88f9ad7282cc8c1a572
yydyzr <liuyuncong1@huawei.com>
2026-07-07 16:01:24 +0800

    fix d2h address computation
    Signed-off-by: yydyzr <liuyuncong1@huawei.com>

--- vllm_ascend/distributed/kv_transfer/sfa_kv_offload/sfa_kv_offload_worker.py
+++ vllm_ascend/distributed/kv_transfer/sfa_kv_offload/sfa_kv_offload_worker.py
@@ -708,22 +708,21 @@ class SFAKVOffloadWorker:
         offload_token_start = self.fused_offload_token_start_cpu[:num_reqs]
         offload_num_tokens = self.fused_offload_num_tokens_cpu[:num_reqs]
 
-        copy_idx = 0
-        for batch_idx in range(num_actual_tokens):
+        def _get_step_offload_addrs(batch_idx: int) -> tuple[int, int, int, int] | None:
             req_idx = int(token_to_req[batch_idx].item())
             if req_idx < 0 or req_idx >= num_reqs:
-                continue
+                return None
             if int(offload_num_tokens[req_idx].item()) <= 0:
-                continue
+                return None
 
             query_start = 0 if req_idx == 0 else int(cum_query_lens[req_idx - 1].item())
             local_offset = batch_idx - query_start
             if local_offset < 0 or local_offset >= int(offload_num_tokens[req_idx].item()):
-                continue
+                return None
 
             slot = int(slots[batch_idx].item())
             if slot < 0:
-                continue
+                return None
 
             global_pos = int(offload_token_start[req_idx].item()) + local_offset
             npu_block_id = slot // self.block_size
@@ -731,33 +730,54 @@ class SFAKVOffloadWorker:
             cpu_block_idx = global_pos // self.block_size
             cpu_block_id = int(cpu_block_table[req_idx, cpu_block_idx].item())
             if cpu_block_id <= 0:
-                continue
+                return None
 
-            self.d2h_gvas_buffer_cpu[copy_idx] = (
+            gva_k = (
                 cpu_k_base
                 + cpu_block_id * block_size_bytes_k
                 + offset_in_block * self.token_size_bytes_k
             )
-            self.d2h_addr_buffer_cpu[copy_idx] = (
+            addr_k = (
                 npu_k_base
                 + npu_block_id * block_size_bytes_k
                 + offset_in_block * self.token_size_bytes_k
             )
-            copy_idx += 1
-            self.d2h_gvas_buffer_cpu[copy_idx] = (
+            gva_v = (
                 cpu_v_base
                 + cpu_block_id * block_size_bytes_v
                 + offset_in_block * self.token_size_bytes_v
             )
-            self.d2h_addr_buffer_cpu[copy_idx] = (
+            addr_v = (
                 npu_v_base
                 + npu_block_id * block_size_bytes_v
                 + offset_in_block * self.token_size_bytes_v
             )
-            copy_idx += 1
+            return gva_k, addr_k, gva_v, addr_v
 
-        num_k_copies = copy_idx // 2
+        v_staging_offset = num_actual_tokens
+        k_idx = 0
+        for batch_idx in range(num_actual_tokens):
+            addrs = _get_step_offload_addrs(batch_idx)
+            if addrs is None:
+                continue
+            gva_k, addr_k, gva_v, addr_v = addrs
+            self.d2h_gvas_buffer_cpu[k_idx] = gva_k
+            self.d2h_addr_buffer_cpu[k_idx] = addr_k
+            staging_idx = v_staging_offset + k_idx
+            self.d2h_gvas_buffer_cpu[staging_idx] = gva_v
+            self.d2h_addr_buffer_cpu[staging_idx] = addr_v
+            k_idx += 1
+
+        num_k_copies = k_idx
+        copy_idx = num_k_copies * 2
         if num_k_copies > 0:
+            if num_k_copies < num_actual_tokens:
+                self.d2h_gvas_buffer_cpu[num_k_copies:copy_idx] = (
+                    self.d2h_gvas_buffer_cpu[v_staging_offset:v_staging_offset + num_k_copies]
+                )
+                self.d2h_addr_buffer_cpu[num_k_copies:copy_idx] = (
+                    self.d2h_addr_buffer_cpu[v_staging_offset:v_staging_offset + num_k_copies]
+                )
             self.d2h_size_buffer_cpu[:num_k_copies].fill_(self.token_size_bytes_k)
             self.d2h_size_buffer_cpu[num_k_copies:copy_idx].fill_(self.token_size_bytes_v)
         self.d2h_num_tokens_buffer_cpu[0] = copy_idx
